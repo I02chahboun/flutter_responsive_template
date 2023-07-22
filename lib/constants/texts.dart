7379f02bda6625d776532af1c496475108412b06
I02chahboun <ismail02.dev@gmail.com>
2023-07-22 18:02:15 +0200

    add texts

---
+++ lib/constants/texts.dart
@@ -0,0 +1,27 @@
+class AppTexts {
+  static const String userDev = "Hello, I02Chahboun";
+  static const String categoryDicover = "Discover World";
+  static const String welcome = "Welcome back and explore the world";
+  static const String catg1 = "Popular Places";
+  static const String catg2 = "Recommended";
+  static const String catg3 = "Near Me";
+  static const String viewAll = "View All";
+  static const String userOne = "El-fouad";
+  static const String userTwo = "M97Chahboun";
+  static const String userTree = "I02Chahboun";
+  static const String userOneDesc = "developer web full stack";
+  static const String userTwoDesc = "developer mobile full stack";
+  static const String userTreeDesc = "developer mobile front-end";
+  static const String categoryEvent = "Event Dates";
+  static const String dashboard = "Dashboard";
+  static const String discover = "Discover";
+  static const String tickets = "Tickets";
+  static const String favorite = "Favorite";
+  static const String message = "Message";
+  static const String transaction = "Transaction";
+  static const String setting = "Settings";
+  static const String titleLogo = "Travenly";
+  static const String myProfile = "My Profile";
+  static const String destination = "Destination Trip";
+  static const String logOut = "Logout";
+}
