b8f317bea051454a2242011f192e96f15fcdbbac
I02chahboun <ismail02.dev@gmail.com>
2023-07-22 17:33:47 +0200

    add assets class

---
+++ lib/constants/images.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  static const String nature1 = "assets/nature1.jpg";
+  static const String nature2 = "assets/nature2.jpg";
+  static const String snow1 = "assets/snow1.jpg";
+  static const String snow2 = "assets/snow2.jpg";
+  static const String storyNature1 = "assets/story_nature1.jpg";
+  static const String storyNature2 = "assets/story_nature2.jpg";
+  static const String storyNature3 = "assets/story_nature3.jpg";
+  static const String backgroundProfile = "assets/backg_profile.jpg";
+}
