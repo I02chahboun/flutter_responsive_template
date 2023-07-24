82bf93c5c2cd76e62719a30139094ff4ea5b7428
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 20:27:26 +0200

    renom buton name

---
+++ lib/widgets/categ_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+import 'package:responsive_home/constants/styles.dart';
+import 'package:responsive_home/constants/texts.dart';
+
+class CategButton extends StatelessWidget {
+  final String title;
+  final Color color;
+  const CategButton(
+      {super.key, required this.title, this.color = AppColors.grey});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isViewAll = title == AppTexts.viewAll;
+    return InkWell(
+      customBorder: RoundedRectangleBorder(
+        borderRadius: Corners.med,
+      ),
+      onTap: () {},
+      child: isViewAll
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Row(
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(color: color, fontSize: 18),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Icon(
+                    Icons.arrow_forward,
+                    color: AppColors.green,
+                    size: 18,
+                  ),
+                ],
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Text(
+                title,
+                style: TextStyle(color: color, fontSize: 18),
+              ),
+            ),
+    );
+  }
+}
