5276db7b4e050416cb70247931e66a62f74da34d
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 15:46:47 +0200

    add profile

---
+++ lib/template/profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.grey,
+    );
+  }
+}
