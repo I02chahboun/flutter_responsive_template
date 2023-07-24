892cd7274c0e2ab95df7c4258f22d406ba77ef6d
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 17:01:36 +0200

    add list tile & search in home

--- lib/template/home.dart
+++ lib/template/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_home/widgets/list_tile.dart';
+import 'package:responsive_home/widgets/search.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,7 +9,11 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        ListTilee(),
+        Expanded(child: ListTilee()),
+        Expanded(child: Search()),
+        Spacer(
+          flex: 3,
+        ),
       ],
     );
   }
