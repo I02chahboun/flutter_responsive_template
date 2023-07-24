97591b44a4e99af5f288510601a4a0d2ac476bc2
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 15:13:51 +0200

    add colors (white & grey)

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color green = Color(0xff008000);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xff808080);
 }
