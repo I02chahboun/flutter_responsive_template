55fd8244f73636352e8979d8ceeb78185c90b2cf
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 15:46:27 +0200

    add drawer

---
+++ lib/template/drawer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.grey,
+    );
+  }
+}
