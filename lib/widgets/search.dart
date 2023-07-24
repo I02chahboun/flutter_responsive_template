1c639f8d417203243c012a355eca2b464e0836f0
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 16:56:51 +0200

    add search templet

---
+++ lib/widgets/search.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/widgets/icon_button.dart';
+import 'package:responsive_home/widgets/elevated_button.dart';
+import 'package:responsive_home/widgets/text_field.dart';
+
+class Search extends StatelessWidget {
+  const Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(flex: 14, child: TextFiled()),
+        Spacer(),
+        Expanded(
+          flex: 2,
+          child: IconButtonn(),
+        ),
+        Spacer(),
+        Expanded(flex: 5, child: ElevatedButtonn()),
+      ],
+    );
+  }
+}
