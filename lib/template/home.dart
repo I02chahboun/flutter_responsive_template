5bf50656b8e9080550bb1e8994cf68e488de5e47
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 15:46:35 +0200

    add home

---
+++ lib/template/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/widgets/list_tile.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        ListTilee(),
+      ],
+    );
+  }
+}
