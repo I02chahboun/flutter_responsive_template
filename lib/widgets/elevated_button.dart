9def5ee114480633da28bd22af7eb46815eaf208
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 16:55:04 +0200

    add elevated button

---
+++ lib/widgets/elevated_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+import 'package:responsive_home/constants/styles.dart';
+import 'package:responsive_home/constants/texts.dart';
+
+class ElevatedButtonn extends StatelessWidget {
+  const ElevatedButtonn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(AppColors.green),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+            borderRadius: Corners.sm,
+          )),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Text(
+            AppTexts.searchButton,
+            style: TextStyles.titleButton,
+          ),
+        ));
+  }
+}
