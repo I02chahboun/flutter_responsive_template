90144a42481fe9af65f814cbfdbfff0c2fa12a5a
I02chahboun <ismail02.dev@gmail.com>
2023-07-24 15:14:04 +0200

    add text styles

---
+++ lib/constants/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+
+class TextStyles {
+  static TextStyle titleStyle =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  static TextStyle descStyle = const TextStyle(
+      fontSize: 17, fontWeight: FontWeight.w600, color: AppColors.grey);
+  static TextStyle catgStyle = const TextStyle(fontSize: 17);
+}
