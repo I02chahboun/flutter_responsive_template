4c4d8489e3c1627884145b4ab7b4a7200195a5d5
I02chahboun <ismail02.dev@gmail.com>
2023-07-25 19:28:37 +0200

    add date button

---
+++ lib/widgets/date_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/colors.dart';
+
+class DateButton extends StatelessWidget {
+  final String day;
+  final int nume;
+  const DateButton({super.key, required this.day, required this.nume});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      customBorder: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(30), bottom: Radius.circular(30)),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 5),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+              top: Radius.circular(30), bottom: Radius.circular(30)),
+        ),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Text(
+            day,
+            style: const TextStyle(fontSize: 12, color: AppColors.grey),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            nume.toString(),
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+                color: AppColors.black),
+          ),
+        ]),
+      ),
+    );
+  }
+}
