24eff6191e4011bed01ec9463792362d73752265
I02chahboun <ismail02.dev@gmail.com>
2023-07-25 19:26:49 +0200

    add black color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color white = Color(0xffFFFFFF);
   static const Color grey = Color(0xff808080);
   static const Color red = Color(0xffF95E5E);
+  static const Color black = Color(0xff000000);
 }
