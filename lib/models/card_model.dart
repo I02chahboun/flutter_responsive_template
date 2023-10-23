009f2eaf1b26523c94b304e587b252f848d74a69
I02chahboun <ismail02.dev@gmail.com>
2023-10-24 00:55:51 +0200

    add event data

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -7,13 +7,20 @@ class CardModel {
   CardModel(this.image, this.title, this.subTitle, this.price);
 }
 
-final List<CardModel> cards = [
+final List<CardModel> discoverData = [
   CardModel(
       AppAssets.storyNature1, AppTexts.userOne, AppTexts.userOneDesc, 200),
   CardModel(
       AppAssets.storyNature2, AppTexts.userTwo, AppTexts.userTwoDesc, 200),
   CardModel(
       AppAssets.storyNature3, AppTexts.userTree, AppTexts.userTreeDesc, 200),
+];
+
+final List<CardModel> eventData = [
+  CardModel(AppAssets.nature1, AppTexts.nature1, AppTexts.userOneDesc, 320),
+  CardModel(AppAssets.nature2, AppTexts.nature2, AppTexts.userTwoDesc, 560),
+  CardModel(AppAssets.snow1, AppTexts.snow1, AppTexts.userTreeDesc, 440),
+  CardModel(AppAssets.snow2, AppTexts.snow2, AppTexts.userTreeDesc, 680),
 ];
 
 List<String> categoryDicover = [
