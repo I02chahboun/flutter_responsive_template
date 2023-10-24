823d8064fd09bbe4c824c3b6373c9daa95461177
I02chahboun <ismail02.dev@gmail.com>
2023-10-24 22:00:18 +0200

    add favorite logic

--- lib/widgets/favorite_icon.dart
+++ lib/widgets/favorite_icon.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_home/constants/colors.dart';
 
-class FavoriteButton extends StatelessWidget {
+// ignore: must_be_immutable
+class FavoriteButton extends StatefulWidget {
   const FavoriteButton({super.key});
 
   @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorited = false;
+  @override
   Widget build(BuildContext context) {
     return IconButton.filled(
-      onPressed: () {},
-      icon: const Icon(
-        Icons.favorite_border,
-        color: AppColors.green,
+      onPressed: () {
+        isFavorited = !isFavorited;
+        setState(() {});
+      },
+      icon: Icon(
+        isFavorited ? Icons.favorite : Icons.favorite_border,
+        color: isFavorited ? AppColors.white : AppColors.green,
         size: 16,
       ),
       style: ButtonStyle(
         minimumSize: MaterialStateProperty.all(Size.zero),
-        backgroundColor:
-            MaterialStateProperty.all<Color>(AppColors.green.withOpacity(0.4)),
+        backgroundColor: MaterialStateProperty.all<Color>(
+            isFavorited ? AppColors.green : AppColors.green.withOpacity(0.2)),
       ),
     );
   }
