a80dcfc70e13eb5c18f198569d30c40583d97a8e
I02chahboun <ismail02.dev@gmail.com>
2023-10-25 23:59:28 +0200

    add responsive sizes

---
+++ lib/constants/breakpoints.dart
@@ -0,0 +1,3 @@
+const double smallBreakpoint = 640;
+const double meduimBreakpoint = 768;
+const double largeBreakpoint = 1200;
---
+++ lib/utils/extensions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/breakpoints.dart';
+
+extension BuildContextExtensions on BuildContext {
+  double get height => MediaQuery.of(this).size.height;
+  double get width => MediaQuery.of(this).size.width;
+  bool get isLarge => width >= largeBreakpoint;
+  bool get isSmall => width < smallBreakpoint;
+  bool get isMeduim => width < largeBreakpoint && width >= smallBreakpoint;
+  bool get inMeduimAndLarge => isMeduim || isLarge;
+  bool get inMeduimAndSmall => isSmall || isMeduim;
+  bool get inLargeAndSmall => isSmall || isLarge;
+  bool get inLarge => isLarge;
+  int get getGridViewCount {
+    if (isSmall) {
+      return 1;
+    } else if (isMeduim) {
+      return 2;
+    } else {
+      return 4;
+    }
+  }
+}
