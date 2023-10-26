647463df1c8ad4233ccab1e978430c63cc5ea349
I02chahboun <ismail02.dev@gmail.com>
2023-10-26 09:20:27 +0200

    add scaffold

--- lib/template/profile.dart
+++ lib/template/profile.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_home/constants/colors.dart';
+import 'package:responsive_home/constants/styles.dart';
+import 'package:responsive_home/constants/texts.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.grey,
+    return Scaffold(
+      body: Container(
+        color: AppColors.grey,
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [Text(AppTexts.myProfile, style: TextStyles.title)]),
+        ),
+      ),
     );
   }
 }
