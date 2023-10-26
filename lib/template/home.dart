e78c14f742ab9795221ca435222c50da65c73cd9
I02chahboun <ismail02.dev@gmail.com>
2023-10-26 08:14:33 +0200

    improve padding

--- lib/template/home.dart
+++ lib/template/home.dart
@@ -12,32 +12,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: AppColors.greenAccent,
-      child: SafeArea(
-        child: Container(
-          height: Sizes.height(context),
-          margin: context.isSmall ? null : const EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            borderRadius: Corners.med,
-            color: AppColors.greenAccent,
-          ),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: context.isSmall ? 10.0 : 20.0, vertical: 10),
-              child: const Column(
-                children: [
-                  ListTilee(),
-                  Search(),
-                  SizedBox(height: 30),
-                  CategDiscover(),
-                  SizedBox(height: 20),
-                  CategEvent(),
-                ],
-              ),
-            ),
-          ),
+    return Container(
+      height: Sizes.height(context),
+      margin: context.isSmall ? null : const EdgeInsets.all(18),
+      padding: EdgeInsets.symmetric(
+          horizontal: context.isSmall ? 10.0 : 20.0, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: Corners.med,
+        color: AppColors.greenAccent,
+      ),
+      child: const SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTilee(),
+            Search(),
+            SizedBox(height: 30),
+            CategDiscover(),
+            SizedBox(height: 20),
+            CategEvent(),
+          ],
         ),
       ),
     );
