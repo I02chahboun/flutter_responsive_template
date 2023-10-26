e5a53d71d3ecc2420974136c2cb30c3836478793
I02chahboun <ismail02.dev@gmail.com>
2023-10-26 09:10:07 +0200

    add logo list tile

---
+++ lib/widgets/logo_list_tile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_home/constants/images.dart';
+import 'package:responsive_home/constants/styles.dart';
+import 'package:responsive_home/constants/texts.dart';
+
+class LogoListTile extends StatelessWidget {
+  const LogoListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(AppAssets.logo, fit: BoxFit.contain, height: 70, width: 70),
+        Text(AppTexts.titleLogo, style: TextStyles.title),
+      ],
+    );
+  }
+}
