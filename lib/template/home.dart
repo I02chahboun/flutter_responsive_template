bdcfa2e15bffa9f8f5651a24623286756c8b5467
I02chahboun <ismail02.dev@gmail.com>
2023-10-27 17:12:27 +0200

    add scaffold & improve drawer

--- lib/template/home.dart
+++ lib/template/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_home/constants/colors.dart';
 import 'package:responsive_home/constants/styles.dart';
+import 'package:responsive_home/template/drawer.dart';
 import 'package:responsive_home/utils/extensions.dart';
 import 'package:responsive_home/widgets/category_discover.dart';
 import 'package:responsive_home/widgets/category_event.dart';
@@ -12,26 +13,42 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: Sizes.height(context),
-      margin: context.isSmall ? null : const EdgeInsets.all(18),
-      padding: EdgeInsets.symmetric(
-          horizontal: context.isSmall ? 10.0 : 20.0, vertical: 10),
-      decoration: BoxDecoration(
-        borderRadius: Corners.med,
-        color: AppColors.greenAccent,
-      ),
-      child: const SingleChildScrollView(
-        child: Column(
-          children: [
-            ListTilee(),
-            SizedBox(height: 7),
-            Search(),
-            SizedBox(height: 20),
-            CategDiscover(),
-            SizedBox(height: 20),
-            CategEvent(),
-          ],
+    return ColoredBox(
+      color: AppColors.greenAccent,
+      child: SafeArea(
+        child: Scaffold(
+          drawer: context.isSmall
+              ? Drawer(
+                  backgroundColor: AppColors.white,
+                  width: context.width / 1.8,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                  child: const MyDrawer(),
+                )
+              : null,
+          backgroundColor: AppColors.greenAccent,
+          body: Container(
+            height: context.height,
+            margin: context.isSmall ? null : const EdgeInsets.all(18),
+            padding: EdgeInsets.symmetric(
+                horizontal: context.isSmall ? 10.0 : 20.0, vertical: 10),
+            decoration: BoxDecoration(
+              borderRadius: context.isSmall ? null : Corners.med,
+            ),
+            child: const SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListTilee(),
+                  SizedBox(height: 7),
+                  Search(),
+                  SizedBox(height: 20),
+                  CategDiscover(),
+                  SizedBox(height: 20),
+                  CategEvent(),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
